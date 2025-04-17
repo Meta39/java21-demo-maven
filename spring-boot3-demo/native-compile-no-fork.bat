2bf63af67f6d8887f0bfaef0a21c09483bbe3b15
Meta39 <53995537@qq.com>
2025-04-17 23:34:25 +0800

    native打包命令优化

--- spring-boot3-demo/native-compile-no-fork.bat
+++ spring-boot3-demo/native-compile-no-fork.bat
@@ -10,10 +10,10 @@ set THREAD=1C
 @echo 如果JAVA_HOME和mvn不一致，则手动修改jdk版本。如果打包失败，说明这里set JAVA_HOME好像没什么用，还是得设置环境变量为graalvm的地址。
 set JAVA_HOME=C:/Users/X/.jdks/graalvm-jdk-21.0.6
 
-@echo 用于启动运行 Maven 的 JVM 的参数
-set MAVEN_OPTS= -Xms2048m -Xmx2048m -XX:MaxMetaspaceSize=512m -XX:ReservedCodeCacheSize=512m
+@echo 用于启动运行 Maven 的 JVM 的参数。GraalVM Native Image 需要大量内存（通常 ≥4GB）。
+set MAVEN_OPTS= -Xms4g -Xmx4g -XX:MaxMetaspaceSize=512m -XX:ReservedCodeCacheSize=512m
 
-set PACKAGE_CMD=mvn clean compile spring-boot:process-aot native:compile-no-fork -f pom.xml -P%PROFILES%
+set PACKAGE_CMD=mvn clean compile spring-boot:process-aot native:compile -DskipTests -Dmaven.compile.fork=true -T %THREAD% -f pom.xml -P%PROFILES%
 echo %PACKAGE_CMD% ......
 start /B /WAIT cmd /c "%PACKAGE_CMD%"
 
